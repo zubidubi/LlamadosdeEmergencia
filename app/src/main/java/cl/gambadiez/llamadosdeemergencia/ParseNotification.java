ca8c5ce1e1048831904c1b2ee9c5248e83349ee6
Agallardol <agallardol@outlook.com>
2015-04-13 15:44:13 -0400

    Implementadas las notificaciones V1

--- app/src/main/java/cl/gambadiez/llamadosdeemergencia/ParseNotification.java
+++ app/src/main/java/cl/gambadiez/llamadosdeemergencia/ParseNotification.java
@@ -1,6 +1,14 @@
 package cl.gambadiez.llamadosdeemergencia;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.PushService;
+import com.parse.SaveCallback;
+
 
 /**
  * Created by Alfredo on 11-04-2015.
@@ -12,6 +20,7 @@ import android.app.Application;
         super.onCreate();
 
         // Add your initialization code here App ID -- Client Key
+        Parse.enableLocalDatastore(this);
         Parse.initialize(this, "r20i16dy7yI4xph20fI2mqYlRDtPoOa5uccl0NUt", "dAiO5yi94O0Sl6EiHCaKxH27yVJBYyM3nTDQhm81");
 
         ParsePush.subscribeInBackground("", new SaveCallback() {
@@ -24,14 +33,5 @@ import android.app.Application;
                 }
             }
         });
-/*
-        ParseUser.enableAutomaticUser();
-        ParseACL defaultACL = new ParseACL();
-
-        // If you would like all objects to be private by default, remove this line.
-        defaultACL.setPublicReadAccess(true);
-
-        ParseACL.setDefaultACL(defaultACL, true);
-        */
     }
 }
